bb3812222068ff07aad7989f2296c269601a4099
higorasilverio <higorasilverio@outlook.com>
2020-04-11 16:44:56 -0300

    Square and the cube of a number

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Square and the cube of a number ***");
+		System.out.println();
+		
+		System.out.print("Enter the number to calculate its square and its cube: ");
+		int number = scanner.nextInt();
+		for (int i = 1; i <= number; i++) {
+			System.out.printf("%d\t%d\t%d\n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+		}
+		
+		scanner.close();
+
+	}
+
+}
